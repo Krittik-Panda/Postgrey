b7f3406b759a5e507c3133179c93b2e92c02787b
Rik <krittikpanda1@gmail.com>
2026-05-02 21:09:58 +0530

    complete CRUD file

--- CURD.sql
+++ CURD.sql
@@ -9,4 +9,35 @@ CREATE TABLE person(
 
 SELECT * FROM person;
 
+--insert into table person
+INSERT INTO person(id,name,city)
+VALUES
+(101,'raju','Mumbai'),(102,'biju','Delhi'),(103,'akit','Hydrabad'),(104,'ritu','raychak');
+
+-- look specific column
+SELECT name FROM person;
+
+-- Update rajus city to bangalore
+UPDATE person
+	SET city  = 'Banglore'
+WHERE id = 101;
+
+
+-- Delete data from the table
+
+DELETE FROM person
+	WHERE id = 101;
+
+--Delete the whole table
+
+DROP TABLE person;
+
+
+
+
+	
+
+	
+
+
  
